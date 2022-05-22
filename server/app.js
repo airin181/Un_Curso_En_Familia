36bce710490a7f32844c91992f12c71e4451f215
Alexdelaoc <alexdelaoc@gmail.com>
2022-05-22 20:05:25 +0200

    [ADD] connected to Mongo successfully

--- server/app.js
+++ server/app.js
@@ -1,14 +1,26 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const env = require("dotenv").config();
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const app = express();
 
 const port = process.env.PORT || 5000
 
-const router = require("./routes/routes-vincula")
+const router = require("./routes/routes-vincula");
+const mongoDBConnection = require('./config/mongodbConfig');
+
 app.use("/api", router)
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })   
+const init = async () => {
+  try {
+      await mongoDBConnection();
+      app.listen(port, () => {
+          console.log(`App listening on port ${port}...`);
+      })
+  }
+  catch (error) {
+      console.log(error);
+  }
+}
+
+init();
