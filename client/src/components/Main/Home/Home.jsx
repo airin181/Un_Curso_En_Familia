6126c5005b2fedae9c1461cad30b69012dcc06ae
airin181 <98608110+airin181@users.noreply.github.com>
2022-05-22 04:14:41 +0200

    [ADD] 2 home screens (not navbar)

--- client/src/components/Main/Home/Home.jsx
+++ client/src/components/Main/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Intro from './Intro/Intro'
+import Intro from './Intro/Intro';
+import Objectives from "./Objectives/Objectives";
+import Program from './Program/Program'
 
 const Home = () => {
   return <>
   
   <Intro/>
+  <Program/>
+  <Objectives/>
   </>
   ;
 };
