cf9bb4254cfd9391e42a889bdc39afed6aa44830
Alexdelaoc <alexdelaoc@gmail.com>
2022-05-23 17:33:34 +0200

    [FIX] node_modules ignored

--- server/app.js
+++ server/app.js
@@ -3,11 +3,6 @@ require('./utils/MongoDb');
 const path = require('path')
 const express = require('express')
 const app = express()
-const env = require("dotenv").config();
-
-
-
-const port = process.env.PORT || 5000
 
 const port = process.env.PORT || 5000;
 
