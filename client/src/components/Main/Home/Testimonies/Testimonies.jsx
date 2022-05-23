4a6a75786d101cfbcc94a84ab9c452a5529dc6be
airin181 <98608110+airin181@users.noreply.github.com>
2022-05-23 08:32:24 +0200

    [ADD] nav bar

--- client/src/components/Main/Home/Testimonies/Testimonies.jsx
+++ client/src/components/Main/Home/Testimonies/Testimonies.jsx
@@ -6,24 +6,59 @@ import img_video_2 from './../../../../assets/ph2.jpg';
 import img_video_3 from './../../../../assets/ph3.jpg';
 import img_video_4 from './../../../../assets/ph4.jpg';
 import screenshot from './../../../../assets/screenshot.png';
+import Card from './Card/Card'
 
 const Testimonies = () => {
 
 
   const [main, setMain] = useState("https://www.youtube.com/embed/0qX3UWsmc-8");
-  const [image, setImage] = useState("");
-  const [video, setVideo] = useState(true);
+  const [image, setImage] = useState(""); 
+  const [video, setVideo] = useState(true); 
 
-
+//si se hace click en una de las imágenes, el estado video pasa a false y se muestra imagen en grande
   const imageClick = (e) => {
     setImage(e)
     setVideo(false)
   }
 
+  //al revés que arriba
   const handleVideoOnClick = () => {
     setVideo(true)
     setImage("")
   }
+
+
+
+// ::::::::::::::
+
+
+const arrayPrejudices = [
+  {
+    id:1,
+    front:"¿Qué sucede si el menor tiene antecedentes familiares?",
+    back:"El objetivo de esta acción es que el menor pueda volver con su familia, por lo que sus antecedentes son parte de la historia del menor."
+  },
+  {
+    id:2,
+    front:"¿Tienes que ser rico para poder acoger?",
+    back:"No hay que ser millonario ni superhéroe, pero sí saber qué niño puedes acoger. Generalmente son familias de nivel medio."
+  },
+  {
+    id:3,
+    front:"¿Qué sucede si el menor tiene problemas de conducta?",
+    back:"Los menores si están en un ambiente estable, con rutinas, normas y afecto, responderán correctamente a todas las interacciones con su entono."
+  }
+]
+
+
+const paintCards = () => {
+  return arrayPrejudices.map((item, i) => <Card data={item} key={i}/>)
+}
+
+
+
+
+
 
   return <section className="general-testimonies">
 
@@ -39,12 +74,12 @@ const Testimonies = () => {
 
       <div className="video-container">
         {video ? <iframe src={main} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> : ""}
-        {image ? <img src={image} alt="Testimonio" style={{width: "100%"}}/> : ""}
+        {image ? <img src={image} alt="Testimonio" style={{ width: "100%", maxWidth: "500px" }} /> : ""}
       </div>
 
       <div className="testimonies-img">
         <div className="circular--landscape">
-          <img src={img_video_1} alt="Miniatura video testimonio" onClick={(e) => imageClick(e.target.src)} className="circular--portrait" />
+          <img src={img_video_1} alt="Miniatura video testimonio" onClick={(e) => imageClick(e.target.src)} />
         </div>
         <div className="circular--landscape">
           <img src={img_video_2} alt="Miniatura video testimonio" onClick={(e) => imageClick(e.target.src)} />
@@ -55,19 +90,26 @@ const Testimonies = () => {
         <div className="circular--landscape">
           <img src={img_video_4} alt="Miniatura video testimonio" onClick={(e) => imageClick(e.target.src)} />
         </div>
-      <div className="circular--landscape">
-      <img src={screenshot} onClick={() => handleVideoOnClick()}></img>
-       </div>
+        <div className="circular--landscape">
+          <img src={screenshot} onClick={() => handleVideoOnClick()}></img>
+        </div>
       </div>
 
+<div className="cards-arrow-testimonies">
+  
 
-
-
-    </article>
-
+      <div className="scrolling-wrapper">
+      {paintCards()}
+      </div>
+ 
     <article className="arrow_downward-testimonies">
       <img src={arrow_downward} alt="Arrow scroll down" className="arrow_downward" />
     </article>
+
+</div>
+
+    </article>
+
 
   </section>
 
