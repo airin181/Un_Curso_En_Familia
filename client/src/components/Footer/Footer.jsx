772d2382df371097b0ecd3711fad95db382e9c13
airin181 <98608110+airin181@users.noreply.github.com>
2022-05-24 23:10:33 +0200

    [UPDATE] footer info

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,35 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Footer = () => {
+
+
+
   return <div className="general-footer">
 
-<section className="footer-text">
-  <p>CONTACTA CON NOSOTROS</p>
-  <p>aseaf@aseaf.org</p>
-  <a href="tel:+34633738461">+ 34 633 738 461</a>
-  <a href="www.aseaf.org">www.aseaf.org</a>
-</section>
+    <section className="footer-text">
+      <p>CONTACTA CON NOSOTROS</p>
+      <p className=".footer-small-text">aseaf@aseaf.org</p>
+      <a href="tel:+34633738461" className=".footer-small-text">+ 34 633 738 461</a>
+      <a href="www.aseaf.org" className=".footer-small-text">www.aseaf.org</a>
+    </section>
 
-<section>
-<p>RECIBE NUESTRA NEWSLETTER</p>
-<input type="text"></input>
-</section>
+    <section>
+      <p>RECIBE NUESTRA NEWSLETTER</p>
+        <input name="email" id="email-dossier" placeholder="Introduce tu email" />
+    </section>
 
-<section className="rrss-icons">
-  <img src="" alt="" />
-  <img src="" alt="" />
-  <img src="" alt="" />
-  <img src="" alt="" />
-</section>
+    <section className="rrss-icons">
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </section>
 
-<section>
-  <p>© 2022 Programa estatal de acogimiento familiar</p>
-  <span>
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
-  </span>
-</section>
+    <section>
+      <p>© 2022 Programa estatal de acogimiento familiar</p>
+      <span>
+        <a href="">Aviso legal</a>
+        <a href="">Política de privacidad</a>
+        <a href="">Política de cookies</a>
+      </span>
+    </section>
 
 
 
