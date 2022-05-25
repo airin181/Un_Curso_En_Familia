a36ed17524c74cc4f4092b0303d3d9a18fd394d0
airin181 <98608110+airin181@users.noreply.github.com>
2022-05-25 12:25:10 +0200

    [ADD] google tag manager

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 import './styles/styles.scss'
-import Form from "./components/Main/Home/Form";
+import TagManager from 'react-gtm-module'
 
 import { LoginContext } from './context/loginContext';
 import { useState } from 'react';
@@ -20,13 +20,21 @@ function App() {
     setUser
   }
 
+  //GOOGLE TAG MANAGER
+  const tagManagerArgs = {
+      gtmId: 'GTM-P3NX7PZ'
+  }
+  
+  TagManager.initialize(tagManagerArgs)
+
+
+
   return (
     <div>
       <BrowserRouter>
         <LoginContext.Provider value={value}>
           <Header />
           <Main />
-          {/* <Form/> */}
         </LoginContext.Provider>
       </BrowserRouter>
       <Footer />
